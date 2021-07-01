eaecb8f68c7d91db781e1488bb7914964edff25c
LP <luizpaulobborges@gmail.com>
2021-07-01 16:33:42 -0300

    responsive grid

--- src/components/PokemonList/styles.ts
+++ src/components/PokemonList/styles.ts
@@ -4,7 +4,15 @@ export const Container = styled.section`
   ul {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    gap: 2rem;    
+    gap: 2rem;
+
+    @media (max-width: 1080px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media (max-width: 720px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
   }
 `;
 
